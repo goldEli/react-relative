c25f259af06b34106f4fd91e1b8027670342f71d
缪宇 <miaoyu2009@qq.com>
2024-04-07 14:31:17 +0800

    feat: add router

--- react-keep-alive/README.md
+++ react-keep-alive/README.md
@@ -1,3 +1,7 @@
 # React  keep alive
 
+* 核心就是 useOutlet 返回 element，即路由对应的页面
+* 如果是需要缓存的页面，将 element 存起到elementList
+* 将 elementList 中所有的element都渲染出来，display none
+* 基于当前路由将对应的element display block
 
--- react-keep-alive/src/App.jsx
+++ react-keep-alive/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Outlet,
+  Link,
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Layout() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>this is layout</h1>
+      <Outlet />
+    </div>
+  );
 }
 
-export default App
+function Aaa() {
+  return (
+    <div>
+      <h1>this is Aaa</h1>
+      <Link to="/bbb">go to bbb</Link>
+      <br />
+      <Link to="/ccc">go to ccc</Link>
+    </div>
+  );
+}
+
+function Bbb() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>this is Bbb</h1>
+      <Link to="/">go to home</Link>
+      <div>count:{count}</div>
+      <button onClick={() => setCount(count + 1)}>add</button>
+    </div>
+  );
+}
+
+
+function Ccc() {
+  return (
+    <div>
+      <h1>this is Ccc</h1>
+      <Link to="/">go to home</Link>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Aaa />,
+      },
+      {
+        path: "/bbb",
+        element: <Bbb />,
+      },
+      {
+        path: "/ccc",
+        element: <Ccc />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <h1>this is home</h1>
+      <hr />
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+export default App;
